915afff879dfcaefb07a8b043af93f35968153f2
Thiago Ferreira Rôvere <93437697+ThiagoFRovere@users.noreply.github.com>
2026-03-28 15:31:52 -0300

    Add files via upload

---
+++ teatapera/js/data.js
@@ -0,0 +1,83 @@
+// Dados de notícias e eventos
+const newsData = [
+     {
+        id: 6,
+        title: 'Caminhada de Conscientização do Autismo',
+        excerpt: 'Diversos em historias, unidos por um propósito. O que você vai encontar por aqui:  Tenda da Saúde, Recreação e Oficinas, Apresentações, Rodas de conversa, Feira de empreendedores',
+        date: '14 de Abril 2026',
+        category: 'event',
+        image: './img/noticias/arte 6.png',
+        details: {
+            'Data': '14 de Abril 2026',
+            'Horário': '15H inicio',
+            'Local': 'Tapera da base aérea',
+            'Local do inicio': 'Rotula da Tapera',
+            'Local de chegada': 'CEFA - Ponto Final',
+        }
+    },
+    {
+        id: 5,
+        title: 'Roda de conversa aberta ao público.',
+        excerpt: 'Como será que o corpo entende o cansaço e o que fazer para chamar o descanso. ',
+        date: '02 de Abril 2026',
+        category: 'event',
+        image: './img/noticias/arte 5.png',
+        details: {
+            'Dias': 'Todas as quintas-feiras', 
+            'Horário': '19:30H',
+            'Local': 'CEFA - Rua José Olimpio da Silva, 1069',
+            'Palestrante': 'Solange Ezure',
+            'Formação': 'Terapeuta Somática',
+            'Informações': 'Pode trazer as crianças.',
+        }
+    },
+     {
+        id: 4,
+        title: 'Rifa Beneficiente',
+        excerpt: 'Todo valor arrecadado será para a manutenção do nosso projeto.',
+        date: '28 de março 2026',
+        category: 'event',
+        image: './img/noticias/arte 4.png',
+        details: {
+            'Valor': 'R$ 2,00 por número',
+        }
+    },
+    {
+        id: 3,
+        title: 'Programa tampinha do bem',
+        excerpt: 'Guarde as suas tampinhas e contribua para o nosso projeto.',
+        date: '28 de março 2026',
+        category: 'event',
+        image: './img/noticias/arte 3.png',
+        details: {
+            'Local de Entrega': 'CEFA - Rua José Olimpio da Silva, 1069',
+
+        }
+    },
+    {
+        id: 2,
+        title: 'Dia Internacional da Sindrome de Down',
+        excerpt: 'Mais amor, mais inclusão.',
+        date: '21 de março 2026',
+        category: 'news',
+        image: './img/noticias/arte 1.png',
+        details: {
+            'Data': '21 de março 2026',
+        }
+    },
+    {
+        id: 1,
+        title: 'Dia das Mulheres',
+        excerpt: 'Neste dia celebramos a força invisível, o amor incondicional e a coragem inabalável das mães e mulheres atípicas.',
+        date: '8 de março 2026',
+        category: 'news',
+        image: './img/noticias/arte 2.png',
+        details: {
+            
+        }
+
+    },
+
+];
+
+
