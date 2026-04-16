178250a3ffe72fc4a0e107825bffdae2deba4123
Thiago Ferreira Rôvere <93437697+ThiagoFRovere@users.noreply.github.com>
2026-04-16 19:14:13 -0300

    Delete gaia directory

--- gaia/js/supabase-config.js
+++
@@ -1,6 +0,0 @@
-// Configurações do Supabase
-const SUPABASE_URL = 'https://kfmqdmawaylnzyypxdxq.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_cBo_sChQ0FhFPVGUnTXmfQ_54M_tOjB';
-
-// Inicializar cliente Supabase
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
