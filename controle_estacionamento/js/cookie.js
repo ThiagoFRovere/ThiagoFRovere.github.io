01942bf1765bb52b58a5d2981512569d5f8f95cb
Thiago Ferreira Rôvere <93437697+ThiagoFRovere@users.noreply.github.com>
2026-07-07 08:09:32 -0300

    Delete controle_estacionamento directory

--- controle_estacionamento/js/cookie.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
-        const url = new URL(link.href);
-        url.searchParams.set('v', Date.now());
-        link.href = url.toString();
-    });
-});
